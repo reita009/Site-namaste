5b8b9a0e3d02cc735ddbd3123dee3a07e3fac6a8
Julio <julio.python777@gmail.com>
2022-01-05 15:04:03 -0300

    terminando primeira sessão

---
+++ namaste-sushi/src/app/first-section/first-section.component.ts
@@ -0,0 +1,20 @@
+import { query } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-section',
+  templateUrl: './first-section.component.html',
+  styleUrls: ['./first-section.component.css']
+})
+export class FirstSectionComponent implements OnInit {
+
+
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
